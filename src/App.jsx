70099e2f861b7855532a6daea28c947d1f189b68
Md.Foysal Ahammed <md.foysalahammedforhad@gmail.com>
2024-03-14 14:20:19 +0600

    working hover/state

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+
+function App() {
+
+  return (
+    <>
+      <div className="h-screen flex justify-center items-center bg-gray-200">
+        <div className="max-w-sm mx-auto p-8 bg-white rounded-xl shadow-md space-y-6 sm:flex sm:items-center sm:py-4 sm:space-y-0 sm:space-x-7 sm:flex-shrink-0">
+          <img className="h-24 mx-auto rounded-full ring-4" src="https://avatars.githubusercontent.com/u/22427255?s=400&u=5b94b3f244d001949d46062f423add93e82a158c&v=4" alt="" />
+          <div className="text-center space-y-2 sm:text-left">
+            <div className="space-y-0.5">
+              <p className="text-2xl text-black font-semibold">Learn with Foysal</p>
+              <p className="text-gray-500 font-medium">Youtube Channel</p>
+            </div>
+            <button className="px-4 py-2 border border-purple-200 rounded-full text-lg text-purple-600 font-semibold">Visit Now</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
